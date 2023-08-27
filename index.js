1150f053ae727eb2eb0924d17ac793807ed39de9
parveen232 <parveen297m210@gmail.com>
2023-08-27 16:18:11 +0530

    add subreddit & not found template

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const redditData = require('./data.json')
+// console.log(redditData); 
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
 app.get('/', (req, res) => {
     res.render('home.ejs')
+})
+
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    const data = redditData[subreddit]
+    // console.log(data)
+    if(data) {
+        res.render('subreddit', { ...data })
+    } else {
+        res.render('notfound', { subreddit })
+    }
+})
+
+app.get('*', (req, res) => {
+    res.send("<h1>I don't know that path</h1>")
 })
 
 app.listen(3000, () => {
