68a93096782c5f28b243062e510b957339ce0e8e
parveen kumar <parveen297m210@gmail.com>
2023-08-28 18:05:40 +0530

    installing tailwindcss

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const redditData = require('./data.json')
-// console.log(redditData); 
+const path = require("path");
+const redditData = require("./data.json");
+// console.log(redditData);
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-    res.render('home.ejs')
-})
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
 
-app.get('/r/:subreddit', (req, res) => {
-    const { subreddit } = req.params;
-    const data = redditData[subreddit]
-    // console.log(data)
-    if(data) {
-        res.render('subreddit', { ...data })
-    } else {
-        res.render('notfound', { subreddit })
-    }
-})
+app.get("/r/:subreddit", (req, res) => {
+  const { subreddit } = req.params;
+  const data = redditData[subreddit];
+  // console.log(data)
+  if (data) {
+    res.render("subreddit", { ...data });
+  } else {
+    res.render("notfound", { subreddit });
+  }
+});
 
-app.get('*', (req, res) => {
-    res.send("<h1>I don't know that path</h1>")
-})
+app.get("*", (req, res) => {
+  res.send("<h1>I don't know that path</h1>");
+});
 
 app.listen(3000, () => {
-    console.log('listening on port 3000');
-})
+  console.log("listening on port 3000");
+});
